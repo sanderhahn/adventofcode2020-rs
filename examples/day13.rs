2d91b6b01b6afbd64a2156d6c60be3581fa3483e
Sander Hahn <sanderhahn@gmail.com>
2020-12-14 14:12:58 +0100

    day 13 b

--- examples/day13.rs
+++ examples/day13.rs
@@ -29,6 +29,42 @@ fn day13a(input: &str) -> usize {
     panic!("error")
 }
 
+fn day13b(input: &str) -> usize {
+    let mut lines = input.lines();
+    let _ = lines.next();
+    let mut busses: Vec<(usize, usize)> = lines
+        .next()
+        .unwrap()
+        .split(",")
+        .enumerate()
+        .map(|(pos, bus)| {
+            if bus == "x" {
+                (pos, 0)
+            } else {
+                (pos, bus.parse().unwrap())
+            }
+        })
+        .filter(|(_, bus)| *bus != 0)
+        .collect();
+
+    let mut step = 1;
+    let mut t = 0;
+    'l: loop {
+        for i in 0..busses.len() {
+            let (pos, bus) = busses[i];
+            if (t + pos) % bus == 0 {
+                step *= bus;
+                busses.remove(i);
+                if busses.len() == 0 {
+                    return t;
+                }
+                continue 'l;
+            }
+        }
+        t += step;
+    }
+}
+
 fn main() {
     let input = include_str!("../inputs/day13.txt");
 
@@ -39,4 +75,12 @@ fn main() {
     assert_eq!(295, day13a(example));
 
     println!("{}", day13a(input));
+
+    let example = "\
+1
+17,x,13,19
+";
+    assert_eq!(3417, day13b(example));
+
+    println!("{}", day13b(input));
 }
